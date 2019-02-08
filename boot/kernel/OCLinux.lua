9bf037f543e2a7a35848d3acd8dcfe57ec3b3372
Wattana Gaming <artjang301@gmail.com>
2019-02-08 16:01:43 +0700

    Cleanup in progress

--- boot/kernel/OCLinux.lua
+++ boot/kernel/OCLinux.lua
@@ -1,28 +1,19 @@
 -- OCLinux kernel by WattanaGaming
--- Clean start is always gud for your health ;)
+-- Set up variables
 _G.boot_invoke = nil
--- Kernel metadata
 _G._KERNELNAME = "OCLinux"
-_G._KERNELVER = "0.2.1 beta"
-_G._KERNELVERSION = _KERNELNAME.." ".._KERNELVER
+_G._KERNELVER = "0.2.2 beta"
+_G._KERNELVERSION = _KERNELNAME.._KERNELVER
 
--- Fetch some important goodies
+-- Load up basic libraries
 component = component or require('component')
 computer = computer or require('computer')
 unicode = unicode or require('unicode')
 
-bootDrive = computer.getBootAddress()
+-- Initialize the GPU and the display
 gpu = component.list("gpu")()
 screen = component.list("screen")()
 
--- Set up variables
-cursorPos = {
-    x = 1,
-    y = 1
-}
-screenResolution = {}
-
--- [[ Initialize the display ]]
 function gpuInvoke(op, arg, ...)
     local res = {}
     local n = 1
@@ -39,7 +30,8 @@ function gpuInvoke(op, arg, ...)
 end
 if gpu and screen then
     --component.invoke(gpu, "bind", screen)
-    w, h = component.invoke(gpu, "getResolution")
+    screenRes = {}
+    screenRes.w, screenRes.h = component.invoke(gpu, "getResolution")
     local res = gpuInvoke("getResolution")
     gpuInvoke("setResolution", res)
     gpuInvoke("setBackground", 0x000000)
@@ -52,17 +44,19 @@ if gpu and screen then
     gpuInvoke("fill", res)
     cls = function()gpuInvoke("fill", res)end
 end
-screenResolution.w, screenResolution.h = gpuInvoke("getResolution")
-
--- [[ END OF GPU SECTION ]]
-
+-- display related functions
+-- This will come in handy for managing cursor position
+cursorPos = {
+    x = 1,
+    y = 1
+}
 function printStatus(...)
     for i in string.gmatch(tostring(...), "([^\r\n]+)") do
-        if cursorPos.y > h then
+        if cursorPos.y > screenRes.h then
             -- Why the hell did they use Cartesian Coordinate? WHY?!
-            gpuInvoke("copy", 1, 2, w, h-1, 0, -1)
+            gpuInvoke("copy", 1, 2, screenRes.w, screenRes.h-1, 0, -1)
             gpuInvoke("setForeground", 0x000000)
-            gpuInvoke("fill", 1, h, w, 1, " ")
+            gpuInvoke("fill", 1, screenRes.h, screenRes.w, 1, " ")
             gpuInvoke("setForeground", 0xFFFFFF)
             cursorPos.y = cursorPos.y - 1
         end
@@ -79,54 +73,7 @@ function writeStatus(...)
     end
 end
 
--- A very low level filesystem controller
-function fs(drive, op, arg, ...)
-    return component.invoke(drive, op, arg, ...)
+for a=1,200 do
+    printStatus(a)
 end
-
-function readFile(drive, file)
-    fileHandle = fs(drive, "open", file, "r")
-    fileSize = fs(drive, "size", file)
-    fileContent = fs(drive, "read", fileHandle, fileSize)
-    return fileContent
-end
-
---Declare kernel specific functions
-function execInit(init)
-    writeStatus("Looking for init \""..init.."\"....    ")
-    if fs(bootDrive, "exists", init) then
-        printStatus("Init found!")
-        initC = readFile(bootDrive, init)
-        load(initC, nil, nil, _G)()
-        return true
-    else
-        printStatus("Not here")
-    end
-end
-
-function panic(reason)
-    if not reason then
-        reason = "No reason specified"
-    end
-    printStatus("Kernel panic: "..reason)
-    printStatus("Kernel version: ".._KERNELVER)
-    printStatus("System uptime: "..computer.uptime())
-    printStatus("System halted")
-    computer.beep(1000, .5)
-    computer.beep(1000, .5)
-    computer.beep(1000, 1)
-    while true do
-        computer.pullSignal()
-    end
-end
-
--- [[ YOU BETTER LEFT THESE PARTS UNTOUCHED ]]
--- A Lua version of the kernel loading code from the Wiki page of the
--- kernel panic
-if not execInit("/sbin/init.lua") and not execInit("/etc/init.lua") and not execInit("/bin/init.lua") then
-    panic("Init not found. You are on your own now, good luck!")
-end
-
--- Halt the system, everything should be ok if there is no BSoD
-panic("Init returned")
--- [[ SomeOnE toUCHA My coDe! ]]
+printStatus("Done")
