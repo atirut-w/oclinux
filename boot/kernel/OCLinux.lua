f3f3ca153ff96fdc132648225c84614f93f9242d
Wattana Gaming <artjang301@gmail.com>
2020-06-20 09:45:50 +0700

    Untested sandboxing system

--- boot/kernel/OCLinux.lua
+++ boot/kernel/OCLinux.lua
@@ -4,11 +4,11 @@ _G._KERNELNAME = "OCLinux"
 _G._KERNELVER = "0.3 beta"
 
 -- These are needed to do literally anything.
-component = component or require('component')
-computer = computer or require('computer')
-unicode = unicode or require('unicode')
+local component = component or require('component')
+local computer = computer or require('computer')
+local unicode = unicode or require('unicode')
 
--- Kernel related facilities. Can be accessed through ENVs
+-- Kernel related facilities. Can be accessed through ENVs. TODO: Consider moving these table declarations to the top of where the functions are.
 local display = {
     isInitialized = false
 }
@@ -18,7 +18,7 @@ local internal = {
 }
 
 function display:initialize()
-    if (self.isInitialized) then -- Prevent the function from running once initialized
+    if (self.isInitialized) then
         return false
     else
         self.isInitialized = true
@@ -26,8 +26,28 @@ function display:initialize()
     self.gpu = component.proxy(component.list("gpu")())
 end
 
+function internal.createSandbox(template)
+    template = template or _G
+    local seen = {} -- DO NOT define this inside the function.
+    local function copy(tbl) -- Massive thanks to Ocawesome101 for this loop!
+        local ret = {}
+        for k, v in pairs(tbl) do -- TODO: Make this loop function-independent.
+            if type(v) == "table" and not seen[v] then
+                seen[v] = true
+                ret[k] = copy(v)
+            else
+                ret[k] = v
+            end
+        end
+        return ret
+    end
+    local sandbox = copy(template)
+    sandbox._G = sandbox
+    return sandbox
+end
+
 function internal:initialize() -- This function have to be executed before the kernel can do anything useful.
-    if (self.isInitialized) then -- Prevent the function from running once initialized
+    if (self.isInitialized) then -- Prevent the function from running again once initialized
         return false
     else
         self.isInitialized = true
