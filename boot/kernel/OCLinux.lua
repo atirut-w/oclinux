955e0b29b7d8a4f4269adbf2db771af79604ad5d
Wattana Gaming <artjang301@gmail.com>
2020-10-25 20:15:14 +0700

    Update OCLinux.lua

--- boot/kernel/OCLinux.lua
+++ boot/kernel/OCLinux.lua
@@ -8,9 +8,14 @@ local component = component or require('component')
 local computer = computer or require('computer')
 local unicode = unicode or require('unicode')
 
--- Kernel related facilities. Can be accessed through ENVs. TODO: Consider moving these table declarations to the top of where the functions are.
+-- Kernel related facilities. Can be accessed through ENVs. 
+-- TODO: Consider moving these table declarations to the top of where the functions are.
 local display = {
-    isInitialized = false
+    isInitialized = false,
+    resolution = {
+        x = nil,
+        y = nil
+    }
 }
 local filesystem = {}
 local internal = {
@@ -24,6 +29,27 @@ function display:initialize()
         self.isInitialized = true
     end
     self.gpu = component.proxy(component.list("gpu")())
+    self.resolution.x, self.resolution.y = self.gpu.getResolution()
+end
+
+-- A very basic and barebone system for putting texts on the screen.
+display.simpleBuffer = {
+    lineBuffer = {}
+}
+
+function display.simpleBuffer:updateScreen()
+    for i=1,#self.lineBuffer do
+        display.gpu.set(1, i, self.lineBuffer[i])
+    end
+end
+
+function display.simpleBuffer:line(text)
+    text = text or ""
+    table.insert(self.lineBuffer, tostring(text))
+    if #self.lineBuffer > display.resolution.y then
+        table.remove(self.lineBuffer, 1)
+    end
+    self:updateScreen()
 end
 
 function internal.createSandbox(template)
@@ -49,12 +75,11 @@ end
 function internal:initialize() -- This function have to be executed before the kernel can do anything useful.
     if (self.isInitialized) then -- Prevent the function from running again once initialized
         return false
-    else
-        self.isInitialized = true
     end
     self.bootAddr = computer.getBootAddress()
     
     display:initialize()
+    self.isInitialized = true
 end
 
 internal:initialize()
