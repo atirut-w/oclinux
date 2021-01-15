359808929b3ee9f6d15cec8729223133f3cec6b5
Wattana Gaming <artjang301@gmail.com>
2021-01-15 21:19:24 +0700

    Update OCLinux.lua
    Found a solution on Stackoverflow. I think I wasted a good amount of my time trying to find a way to copy the darn `_G` lmao.

--- boot/kernel/OCLinux.lua
+++ boot/kernel/OCLinux.lua
@@ -150,8 +150,22 @@ kernel.internal = {
     return buffer
   end,
   
-  loadfile = function(file, env)
-    return load(kernel.internal.readfile(file), "=" .. file, "bt", env)
+  loadfile = function(file, env, isSandbox)
+    local function copy(obj, seen)
+      if type(obj) ~= 'table' then return obj end
+      if seen and seen[obj] then return seen[obj] end
+      local s = seen or {}
+      local res = setmetatable({}, getmetatable(obj))
+      s[obj] = res
+      for k, v in pairs(obj) do res[copy(k, s)] = copy(v, s) end
+      return res
+    end
+
+    if isSandbox == true then
+      return load(kernel.internal.readfile(file), "=" .. file, "bt", copy(env))
+    else
+      return load(kernel.internal.readfile(file), "=" .. file, "bt", env)
+    end
   end,
   
   initialize = function(self)
@@ -182,8 +196,6 @@ kernel.internal = {
 }
 
 kernel.internal:initialize()
-print = function(message) kernel.display.simpleBuffer:print(message) end
-bruhhhhh = "yus"
 
 while coroutine.status(kernel.threads.coroutines[1].co) ~= "dead" do
   kernel.threads:cycle()
